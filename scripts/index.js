e6c548834bb485739803be50d9d142523a563ea6
Diego <diegouriarte36@gmail.com>
2025-06-12 16:48:22 -0700

    project 4 part 3 first submission

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,23 @@
 const profileEditBtn = document.querySelector(".profile__edit-btn");
 const profileEditModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseBtn = profileEditModal.querySelector(".modal__close-btn");
+const editProfileNameInput = profileEditModal.querySelector("#name");
+const editProfileForm = profileEditModal.querySelector(".modal__form");
+const editProfileDescInput = profileEditModal.querySelector("#description");
+
 const newPostBtn = document.querySelector(".profile__add-btn");
 const newPostModal = document.querySelector("#new-post-modal");
 const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
+const newPostForm = newPostModal.querySelector(".modal__form");
+const newPostTitleInput = newPostModal.querySelector("#image-caption");
+const newPostImageInput = newPostModal.querySelector("#image-link");
+
+const profileNameEl = document.querySelector(".profile__name");
+const ProfileDescriptionEl = document.querySelector(".profile__description");
 
 profileEditBtn.addEventListener("click", function () {
+  editProfileNameInput.value = profileNameEl.textContent;
+  editProfileDescInput.value = ProfileDescriptionEl.textContent;
   profileEditModal.classList.add("modal_is-opened");
 });
 
@@ -20,3 +32,20 @@ newPostBtn.addEventListener("click", function () {
 newPostCloseBtn.addEventListener("click", function () {
   newPostModal.classList.remove("modal_is-opened");
 });
+
+function handleEditProfileSubmit(evt) {
+  evt.preventDefault();
+  profileNameEl.textContent = editProfileNameInput.value;
+  ProfileDescriptionEl.textContent = editProfileDescInput.value;
+  profileEditModal.classList.remove("modal_is-opened");
+}
+
+editProfileForm.addEventListener("submit", handleEditProfileSubmit);
+
+function handleNewPostSubmit(evt) {
+  evt.preventDefault();
+  console.log(newPostImageInput.value, newPostTitleInput.value);
+  newPostModal.classList.remove("modal_is-opened");
+}
+
+newPostForm.addEventListener("submit", handleNewPostSubmit);
