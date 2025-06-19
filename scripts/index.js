adcec3e2dde4c2d9303a1e25b1a8d25e6ac00744
Diego <diegouriarte36@gmail.com>
2025-06-19 02:17:55 -0700

    project 4 stage 1

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,28 @@
+const initialCards = [{
+  name: "Val Thorens"
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg",
+},
+{
+  name: "Restaurant terrace"
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg",
+},
+{
+  name: "An outdoor cafe"
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg",
+},
+{
+  name: "A very long bridge, over the forest and through the trees"
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg",
+},
+{
+  name: "Tunnel with morning light"
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg",
+},
+{
+  name: "Mountain house"
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",
+},]; 
+
 const profileEditBtn = document.querySelector(".profile__edit-btn");
 const profileEditModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseBtn = profileEditModal.querySelector(".modal__close-btn");
@@ -49,3 +74,7 @@ function handleNewPostSubmit(evt) {
 }
 
 newPostForm.addEventListener("submit", handleNewPostSubmit);
+
+initialCards.forEach(function (item) {
+  console.log(item.name, item.link);
+});
