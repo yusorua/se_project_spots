34ecbe0fcb677e3e3a49ffd61049e5c2e4f0ffb9
Diego <diegouriarte36@gmail.com>
2025-06-22 20:18:32 -0700

    project 5 stage 2

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,26 @@
 const initialCards = [{
   name: "Val Thorens"
-  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg"
 },
 {
   name: "Restaurant terrace"
-  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg"
 },
 {
   name: "An outdoor cafe"
-  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg"
 },
 {
   name: "A very long bridge, over the forest and through the trees"
-  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg"
 },
 {
   name: "Tunnel with morning light"
-  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg"
 },
 {
   name: "Mountain house"
-  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg"
 },]; 
 
 const profileEditBtn = document.querySelector(".profile__edit-btn");
@@ -40,29 +40,40 @@ const newPostImageInput = newPostModal.querySelector("#image-link");
 const profileNameEl = document.querySelector(".profile__name");
 const ProfileDescriptionEl = document.querySelector(".profile__description");
 
+function openModal(modal) {
+  modal.classList.add("modal_is-opened");}
+
+function closeModal(modal) {
+  modal.classList.remove("modal_is-opened");}
+
 profileEditBtn.addEventListener("click", function () {
   editProfileNameInput.value = profileNameEl.textContent;
   editProfileDescInput.value = ProfileDescriptionEl.textContent;
-  profileEditModal.classList.add("modal_is-opened");
+  // profileEditModal.classList.add("modal_is-opened");
+  openModal(profileEditModal);
 });
 
 editProfileCloseBtn.addEventListener("click", function () {
-  profileEditModal.classList.remove("modal_is-opened");
+  // profileEditModal.classList.remove("modal_is-opened");
+    openModal(profileEditModal);
 });
 
 newPostBtn.addEventListener("click", function () {
-  newPostModal.classList.add("modal_is-opened");
+  // newPostModal.classList.add("modal_is-opened");
+  openModal(newPostModal);
 });
 
 newPostCloseBtn.addEventListener("click", function () {
-  newPostModal.classList.remove("modal_is-opened");
+  // newPostModal.classList.remove("modal_is-opened");
+  closeModal(newPostModal);
 });
 
 function handleEditProfileSubmit(evt) {
   evt.preventDefault();
   profileNameEl.textContent = editProfileNameInput.value;
   ProfileDescriptionEl.textContent = editProfileDescInput.value;
-  profileEditModal.classList.remove("modal_is-opened");
+  // profileEditModal.classList.remove("modal_is-opened");
+  closeModal(profileEditModal);
 }
 
 editProfileForm.addEventListener("submit", handleEditProfileSubmit);
@@ -70,7 +81,8 @@ editProfileForm.addEventListener("submit", handleEditProfileSubmit);
 function handleNewPostSubmit(evt) {
   evt.preventDefault();
   console.log(newPostImageInput.value, newPostTitleInput.value);
-  newPostModal.classList.remove("modal_is-opened");
+  // newPostModal.classList.remove("modal_is-opened");
+  closeModal(newPostModal);
 }
 
 newPostForm.addEventListener("submit", handleNewPostSubmit);
